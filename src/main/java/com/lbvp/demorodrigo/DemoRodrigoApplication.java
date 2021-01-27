9edaf5f5ac44f8d21cc8e729f537a384c505ce41
luisvalle <lvallep@indracompany.com>
2021-01-27 10:51:28 -0500

    first commit

---
+++ src/main/java/com/lbvp/demorodrigo/DemoRodrigoApplication.java
@@ -0,0 +1,13 @@
+package com.lbvp.demorodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRodrigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRodrigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lbvp/demorodrigo/DemoRodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbvp.demorodrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRodrigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
